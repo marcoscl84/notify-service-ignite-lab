379b141e5dad88c446c0d84226369f8e6eb65683
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2022-12-17 10:11:57 -0300

    feat: colocação módulo de exemplo do início do projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './infra/app.controller';
+import { HttpModule } from './infra/http.module';
 
 import { PrismaService } from './infra/prisma.service';
 
 @Module({
-  imports: [],
+  imports: [HttpModule],
   controllers: [AppController],
   providers: [PrismaService],
 })
