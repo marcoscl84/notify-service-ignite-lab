f9aab4c8504753c82c908e68603c28b2319a1030
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2022-12-18 10:57:14 -0300

    feat: simulação de banco de dados para testar a aplicação

--- src/app/useCases/send-notification-useCase.spec.ts
+++ src/app/useCases/send-notification-useCase.spec.ts
@@ -1,15 +1,31 @@
 import { SendNotificationUseCase } from './send-notification-useCase';
+import { Notification } from '../entities/notification';
+
+// simula uma tabela no banco
+let notifications: Notification[] = [];
+
+// insere novo dado na pseudo tabela
+const notificationsRepository = {
+  async create(notification: Notification) {
+    console.log(notification);
+    notifications.push(notification);
+  },
+};
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotificationUseCase();
+    const sendNotification = new SendNotificationUseCase(
+      notificationsRepository,
+    );
 
-    const { notification } = await sendNotification.execute({
+    /* const { notification } = */ await sendNotification.execute({
       category: 'social',
-      content: 'THis is a notification',
+      content: 'This is a notification',
       recipientId: 'example-recipient-id',
     });
 
-    expect(notification).toBeTruthy();
+    console.log(notifications);
+    expect(notifications).toHaveLength(1);
+    // expect(notification).toBeTruthy();
   });
 });
